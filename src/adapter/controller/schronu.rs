2492ed00c9cd4743c9d452f596fbf9d025ac9386
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2024-05-13 09:58:46 +0900

    Wコマンドの修正に伴い暦コマンドでの前倒し時間の特例を消した

--- src/adapter/controller/schronu.rs
+++ src/adapter/controller/schronu.rs
@@ -484,9 +484,7 @@ fn execute_show_all_tasks(
                 // 前倒し可能なタスクの見積もり時間をカウントする
                 let mut adjustable_prefix_label = "".to_string();
 
-                // 【繰】タスクの中にもアニメのように前倒しできるものはあるが、今はWコマンドのミスによりstart_timeの値が過去の日付のまま更新されず残っているので便宜的に【繰】の場合は前倒しできないとする
-                if repetition_prefix_label == ""
-                    && rank == &0
+                if rank == &0
                     && !task.get_is_on_other_side()
                     && task.get_start_time() < get_next_morning_datetime(*dt) - Duration::days(1)
                 {
