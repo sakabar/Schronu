1c5df0443331e445032c553e2b600d2512e7a200
Takafumi Sakakibara <sakabar@users.noreply.github.com>
2026-06-04 22:14:39 +0900

    スプレッドシートのN列に「スキップ」のフラグを追加

--- shell/copy_for_spreadsheet.sh
+++ shell/copy_for_spreadsheet.sh
@@ -9,15 +9,16 @@ cat - | grep -v -- '^----' | tr ' ' '\t' | tac | awk 'NF >= 9' | while read line
 
     hour="LEFT(MID(E${cell_row_num}, 10, 5), 2)"
     minute="RIGHT(MID(E${cell_row_num}, 10, 5), 2)"
-    now_time='TIME(HOUR($S$2),MINUTE($S$2),SECOND($S$2))'
+    now_time='TIME(HOUR($T$2),MINUTE($T$2),SECOND($T$2))'
 
     manu_cell="=NOT(ISFORMULA(K${cell_row_num}))"
-    k_cell="=MAX(TIME(${hour}, ${minute}, 0), R${prev_cell_row_num}, ${now_time})"
+    k_cell="=MAX(TIME(${hour}, ${minute}, 0), S${prev_cell_row_num}, ${now_time})"
 
     l_cell=''
-    m_cell='='"K${cell_row_num}"'+time(0, '"G${cell_row_num}"', 0)'
+    m_cell='=IF(N'"${cell_row_num}"'="T", K'"${cell_row_num}"', K'"${cell_row_num}"'+TIME(0, G'"${cell_row_num}"', 0))'
+    n_cell=''
 
-    echo ${line}"\t${manu_cell}\t${k_cell}\t${l_cell}\t${m_cell}"
+    echo ${line}"\t${manu_cell}\t${k_cell}\t${l_cell}\t${m_cell}\t${n_cell}"
 
     cell_row_num=$[$cell_row_num + 1]
 done
