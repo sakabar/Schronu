5c98ad2b23f024ed5bff9dddd316e0d65d9a4104
Takafumi Sakakibara <sakabar@users.noreply.github.com>
2026-08-22 16:39:10 +0900

    Refactor: Spreadsheet業務日境界判定を簡潔にする

--- shell/copy_for_spreadsheet.sh
+++ shell/copy_for_spreadsheet.sh
@@ -2,61 +2,8 @@
 # 2024/03/16
 set -ue
 
-function is_next_weekday() {
-    local previous_weekday=$1
-    local current_weekday=$2
-    local expected_weekday
-
-    case ${previous_weekday} in
-        月) expected_weekday=火 ;;
-        火) expected_weekday=水 ;;
-        水) expected_weekday=木 ;;
-        木) expected_weekday=金 ;;
-        金) expected_weekday=土 ;;
-        土) expected_weekday=日 ;;
-        日) expected_weekday=月 ;;
-        *) return 1 ;;
-    esac
-
-    [[ ${current_weekday} == ${expected_weekday} ]]
-}
-
-function is_next_calendar_date() {
-    local previous_date=$1
-    local current_date=$2
-    local previous_weekday=$3
-    local current_weekday=$4
-    local previous_month=${previous_date%%/*}
-    local previous_day=${previous_date#*/}
-    local current_month=${current_date%%/*}
-    local current_day=${current_date#*/}
-
-    is_next_weekday "${previous_weekday}" "${current_weekday}" || return 1
-
-    if (( 10#${previous_month} == 10#${current_month} && 10#${current_day} == 10#${previous_day} + 1 )); then
-        return 0
-    fi
-
-    case ${previous_month} in
-        01|03|05|07|08|10)
-            (( 10#${previous_day} == 31 && 10#${current_month} == 10#${previous_month} + 1 && 10#${current_day} == 1 ))
-            ;;
-        04|06|09|11)
-            (( 10#${previous_day} == 30 && 10#${current_month} == 10#${previous_month} + 1 && 10#${current_day} == 1 ))
-            ;;
-        02)
-            (( (10#${previous_day} == 28 || 10#${previous_day} == 29) && 10#${current_month} == 3 && 10#${current_day} == 1 ))
-            ;;
-        12)
-            (( 10#${previous_day} == 31 && 10#${current_month} == 1 && 10#${current_day} == 1 ))
-            ;;
-        *)
-            return 1
-            ;;
-    esac
-}
-
 cell_row_num=3
+weekday_order='月火水木金土日月'
 cat - | awk '
 /^0/ && !/^----/ {
     line = $0
@@ -98,13 +45,8 @@ cat - | awk '
             if (( previous_scheduled_start_minutes < 360 && scheduled_start_minutes >= 360 )); then
                 sleep_minutes=420
             fi
-        elif (( scheduled_start_minutes >= 360 )); then
-            sleep_minutes=420
-        elif (( previous_scheduled_start_minutes < 360 )) || ! is_next_calendar_date \
-                "${previous_scheduled_date}" \
-                "${scheduled_date}" \
-                "${previous_scheduled_weekday}" \
-                "${scheduled_weekday}"; then
+        elif (( scheduled_start_minutes >= 360 || previous_scheduled_start_minutes < 360 )) ||
+            [[ ${weekday_order} != *${previous_scheduled_weekday}${scheduled_weekday}* ]]; then
             sleep_minutes=420
         fi
     fi
@@ -119,11 +61,9 @@ cat - | awk '
     l_cell="=L${cell_row_num}"
     m_cell=''
     n_cell="=P${cell_row_num}"
-    if (( sleep_minutes > 0 )); then
-        o_cell='=IF(OR(R'"${cell_row_num}"'="W", R'"${cell_row_num}"'="d"), L'"${cell_row_num}"', L'"${cell_row_num}"'+TIME(0, G'"${cell_row_num}"'+'"${sleep_minutes}"', 0))'
-    else
-        o_cell='=IF(OR(R'"${cell_row_num}"'="W", R'"${cell_row_num}"'="d"), L'"${cell_row_num}"', L'"${cell_row_num}"'+TIME(0, G'"${cell_row_num}"', 0))'
-    fi
+    estimated_minutes_formula="G${cell_row_num}"
+    (( sleep_minutes > 0 )) && estimated_minutes_formula+="+${sleep_minutes}"
+    o_cell='=IF(OR(R'"${cell_row_num}"'="W", R'"${cell_row_num}"'="d"), L'"${cell_row_num}"', L'"${cell_row_num}"'+TIME(0, '"${estimated_minutes_formula}"', 0))'
     p_cell="=NOT(ISFORMULA(P${cell_row_num}))"
     q_cell=''
 
