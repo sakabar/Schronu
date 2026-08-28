9300e0cc3a65a65297594cf2bc8665a9e344a6c6
Takafumi Sakakibara <sakabar@users.noreply.github.com>
2026-08-29 00:58:48 +0900

    Refactor: busy時間slotの内部契約を縮小する

--- src/entity/busy_time_slot.rs
+++ src/entity/busy_time_slot.rs
@@ -1,34 +1,27 @@
-pub struct BusyTimeSlot {
+pub(crate) struct BusyTimeSlot {
     start_time_hour: u32,
     start_time_minute: u32,
     duration_minutes: i64,
-    _name: String,
 }
 
 impl BusyTimeSlot {
-    pub fn new(
-        start_time_hour: u32,
-        start_time_minute: u32,
-        duration_minutes: i64,
-        name: String,
-    ) -> Self {
+    pub(crate) fn new(start_time_hour: u32, start_time_minute: u32, duration_minutes: i64) -> Self {
         Self {
             start_time_hour,
             start_time_minute,
             duration_minutes,
-            _name: name,
         }
     }
 
-    pub fn get_start_time_hour(&self) -> u32 {
+    pub(crate) fn get_start_time_hour(&self) -> u32 {
         self.start_time_hour
     }
 
-    pub fn get_start_time_minute(&self) -> u32 {
+    pub(crate) fn get_start_time_minute(&self) -> u32 {
         self.start_time_minute
     }
 
-    pub fn get_duration_minutes(&self) -> i64 {
+    pub(crate) fn get_duration_minutes(&self) -> i64 {
         self.duration_minutes
     }
 }
