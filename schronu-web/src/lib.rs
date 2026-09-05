432a2265b10471ebeb634e99be1e7030e55b2dce
Takafumi Sakakibara <sakabar@users.noreply.github.com>
2026-09-05 18:03:18 +0900

    Web: 5操作を専用workerで直列化する

--- schronu-web/src/lib.rs
+++ schronu-web/src/lib.rs
@@ -4,10 +4,12 @@ pub mod app;
 mod controller_error;
 mod refresh_state;
 mod today_worker;
+mod web_worker;
 mod wire;
 
 pub use refresh_state::{RefreshState, RefreshTrigger, REFRESH_INTERVAL};
 pub use today_worker::{TodayTextQuery, TodayWorkerHandle};
+pub use web_worker::{WebOperations, WebWorkerHandle};
 pub use wire::{
     web_error_codes, CompleteSessionResponse, ListTasksRequest, RecordSessionRequest,
     RecordSessionResult, RetryAdvice, ScheduledTaskRow, ServerSnapshot, SessionTask, WebError,
